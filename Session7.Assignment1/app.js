cc642e73980f4e5f93923ca97afe580ac64e253a
AgnishPOC <agnishshyam@outlook.com>
2016-11-05 10:42:24 +0530

    Add files via upload

---
+++ Session7.Assignment1/app.js
@@ -0,0 +1,7 @@
+        var radius=7;
+        var calculateArea=function (radius)
+        {
+           return(3.14*radius*radius);
+        };
+
+        console.log("Area of the circle :" + Math.round(calculateArea(radius))) ; //Rounded OFF since desired output is 184
